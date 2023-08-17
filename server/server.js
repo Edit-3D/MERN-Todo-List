9fa8a49ee24d579e5f15a6d02cd67a5ed40d05d5
Edit-3D <aadi.nair@gmail.com>
2023-08-17 20:57:21 +0200

    Update server.js
    setup mongodb connetion

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Set up routes and middleware here
+//Connect to MongoDB
+mongoose.connect("mongodb://127.0.0.1/todo-list", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const PORT = process.env.PORT || 5000;
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+// Quick test for backend
+app.get("/api", function (req, res) {
+  res.json({ message: "Todo List Backend" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
